08fa88dc3242679bc02c628da707888d145e24ca
tonioreign <tonioreign@gmail.com>
2022-05-09 09:11:13 -0700

    Added project section, contact form

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const navbar = document.querySelector('.nav__links');
 const nav = document.querySelector('nav');
 const section = document.querySelectorAll('section');
 console;
+const mousea = document.querySelector('.mousea');
 // Functions
 
 // smooth scrolling
@@ -23,7 +24,17 @@ const stickyNav = () => {
   nav.classList.toggle('sticky', window.scrollY > 0);
 };
 
+// mouse scroll
+
 // Event handlers
+
+mousea.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.classList.contains('mousea')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
 
 navbar.addEventListener('click', smoothScroll);
 window.addEventListener('scroll', stickyNav);
